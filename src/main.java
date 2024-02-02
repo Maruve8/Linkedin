8049e19f3d0430e4b4e566de982f0b5a44b00684
Maruve8 <mv.contrerasbellido@gmail.com>
2024-02-02 11:08:58 +0100

    Linkedin

---
+++ src/main.java
@@ -0,0 +1,27 @@
+public class main {
+	public class PortadaLinkedIn {
+
+	    public static void main(String[] args) {
+	        // Mensaje principal para la portada
+	        String mensajePrincipal = "Junior Developer";
+
+	        // Array de líneas de código y mensajes
+	        String[] mensajes = {
+	            "Java",
+	            "HTML & CSS: Creando interfaces web atractivas y responsivas",
+	            "JavaScript: Desarrollando interactividad y dinamismo en la web",
+	            "PHP: Construyendo aplicaciones web robustas y escalables",
+	            "SQL: Modelando y gestionando bases de datos eficientemente"
+	        };
+
+	        // Mostrar mensaje principal
+	        System.out.println(mensajePrincipal);
+
+	        // Mostrar líneas de código y mensajes
+	        for (String mensaje : mensajes) {
+	            System.out.println(mensaje);
+	        }
+	    }
+	}
+
+}
